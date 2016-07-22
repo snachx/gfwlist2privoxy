bcec189fc171c0ebd0ae6479f8c4f9f15836bb7c
Rogerz Zhang <rogerz.zhang@gmail.com>
2016-07-22 21:37:29 +0800

    Fix default url of gfwlist.txt (#2)

--- gfwlist2privoxy/main.py
+++ gfwlist2privoxy/main.py
@@ -11,7 +11,7 @@ from argparse import ArgumentParser
 __all__ = ['main']
 
 
-gfwlist_url = 'https://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt'
+gfwlist_url = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
 
 
 def parse_args():
